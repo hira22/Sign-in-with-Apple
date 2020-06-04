d230cf3d350776347007023a021dfdd56a5276d2
hira22 <masayuki.hiraoka@ietty.co.jp>
2020-06-04 15:47:12 +0900

    Initial Commit

---
+++ Sign-in-with-Apple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sign-in-with-Apple
+//
+//  Created by hiraoka on 2020/06/04.
+//  Copyright © 2020 hiraoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
